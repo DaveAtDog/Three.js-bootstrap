1509e97c88a0d9a265266d67f3202522512873ab
Dave Lochhead <dave.lochhead@dogdigital.com>
2014-11-13 10:56:32 +0000

    Fixed some IE8 errors.

--- js/WebGLDetect.js
+++ js/WebGLDetect.js
@@ -1,6 +1,33 @@
 var Dog = Dog ||
 {};
 
+/**
+ * Protect window.console method calls, e.g. console is not defined on IE
+ * unless dev tools are open, and IE doesn't define console.debug
+ * http://stackoverflow.com/questions/3326650/console-is-undefined-error-for-internet-explorer
+ */
+(function()
+{
+	if (!window.console)
+	{
+		window.console = {};
+	}
+	// union of Chrome, FF, IE, and Safari console methods
+	var m = [
+		"log", "info", "warn", "error", "debug", "trace", "dir", "group",
+		"groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+		"dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+	];
+	// define undefined methods as noops to prevent errors
+	for (var i = 0; i < m.length; i++)
+	{
+		if (!window.console[m[i]])
+		{
+			window.console[m[i]] = function() {};
+		}
+	}
+})();
+
 Dog.WebGLDetect = (function()
 {
 	var canvas, ctx;
@@ -14,7 +41,7 @@ Dog.WebGLDetect = (function()
 	}
 	catch (error)
 	{
-		console.log(error);
+		console.log('Dog.WebGLDetect', error);
 	}
 
 	if (ctx !== undefined && ctx !== null)
@@ -23,7 +50,7 @@ Dog.WebGLDetect = (function()
 	}
 	else
 	{
-		console.info('WebGl unavailable or disabled.');
+		console.log('WebGl unavailable or disabled.');
 	}
 
 	canvas = undefined;
