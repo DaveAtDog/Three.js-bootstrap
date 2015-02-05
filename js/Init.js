bb06b2afa9659c3797fd5670fed8e7ebbbf8e3bd
Dave Lochhead <dave.lochhead@dogdigital.com>
2015-02-05 10:07:11 +0000

    Updated to use Three.js (r70).

--- js/Init.js
+++ js/Init.js
@@ -3,7 +3,7 @@ if (Dog.WebGLDetect.canProceed)
 	// If WebGL is available to use then we load our libraries.
 	LazyLoad.js(
 		[
-			"//cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js",
+			"//cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js",
 			"./js/vendor/OrbitControls.js",
 			"./js/Utils.js",
 			"./js/Main.js"
